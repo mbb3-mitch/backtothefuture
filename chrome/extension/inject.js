83bd69cf8ad4f9388698168a609fcc4a0378d618
Pascal <pgendron40@gmail.com>
2018-09-29 17:29:10 -0400

    Change font

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -3,60 +3,38 @@ import {render} from 'react-dom';
 import Dock from 'react-dock';
 
 class InjectApp extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {isVisible : false};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {isVisible: false};
+    }
 
-	buttonOnClick = () => {
-		this.setState({isVisible : !this.state.isVisible});
-	};
-
-	render() {
-		return (
-			< div >
-			< button
-		onClick = {this.buttonOnClick
-	}>
-		Open
-		app
-		< /button>
-		< Dock
-		position = "right"
-		dimMode = "transparent"
-		defaultSize = {0.4}
-		isVisible = {this.state.isVisible
-	}
-	>
-	<
-		iframe
-		style = {
-		{
-			width: '100%',
-				height
-		:
-			'100%',
-		}
-	}
-		frameBorder = {0}
-		allowTransparency = "true"
-		src = {chrome.extension.getURL(`inject.html?protocol=${location.protocol}`)
-	}
-		/>
-		< /Dock>
-		< /div>
-	)
-		;
-	}
+    buttonOnClick = () => {
+        this.setState({isVisible: !this.state.isVisible});
+    };
 }
+
 
 const async = require('async');
 const Translator = require("../../utils/Translator");
 const translator = new Translator();
 
+function replaceFont() {
+    let fontLink = document.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href = "https://github.com/mbb3-mitch/backtothefuture/blob/ffc102a5b594ceb77addc2a0f9942d1c015e7aa1/media/fonts/OldLondon.ttf?raw=true";
+    document.body.appendChild(fontLink);
+    let newFont = document.createElement('style');
+    newFont.textContent = "      body {\n" +
+        "        font-family: 'Old London' !important;\n" +
+        "      }";
+    document.body.appendChild(newFont);
+}
+
 
 window.addEventListener('load', () => {
-	let all = document.getElementsByTagName("*");
+    replaceFont();
+
+    let all = document.getElementsByTagName("*");
 	let textElements = [];
 
 	for (var i = 0, max = all.length; i < max; i++) {
